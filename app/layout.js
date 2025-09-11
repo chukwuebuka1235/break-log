7ea2e9b205c3ce41301ff5f979e8d3791f501d0b
Charles-patrick <ebukacharles006@gmail.com>
2025-09-11 16:49:06 +0100

    updates

--- app/layout.js
+++ app/layout.js
@@ -1,41 +1,20 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Navbar } from "@/app/layout/navbar";
 import Toast from "./components/Toast";
+
 
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
 });
 
-const baseUrl = process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-
 export const metadata = {
   title: "Break Management System",
   description: "Streamline your workforce management with our intuitive break tracking system. Monitor employee breaks in real-time, generate reports, and optimize productivity.",
   keywords: "break management, employee tracking, productivity, workforce management, break tracker, time management",
   authors: [{ name: "Kadick Integrated Limited" }],
-  openGraph: {
-    title: "Break Management System",
-    description: "Streamline your workforce management with our intuitive break tracking system.",
-    url: baseUrl,
-    siteName: "Break Management System",
-    images: [
-      {
-        url: `${baseUrl}/kadick.png`,
-        width: 1200,
-        height: 630,
-        alt: "Break Management System Preview",
-      },
-    ],
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Break Management System",
-    description: "Streamline your workforce management with our intuitive break tracking system.",
-    images: [`${baseUrl}/kadick.png`], 
+  icons: {
+    icon: "/kadick.png", 
   },
 };
 
